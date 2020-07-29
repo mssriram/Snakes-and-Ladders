cf373db990a271eac3c2e82b3f88a2b117152d9f
mssriram <42005010+mssriram@users.noreply.github.com>
2020-07-29 21:50:48 +0530

    Update and rename snake_ladder.sh to shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -8,16 +8,14 @@ cp slinp.dat temp.dat
 n1=1
 while test $n1 -le $nlines
 do
-read s1 s2 l1 l2< temp.dat
+read snake ladder< temp.dat
 echo $n1 $s1 $s2 $l1 $l2 
 python3 ./snake_ladder.py3 <<%
-$s1
-$s2
-$l1
-$l2
+$snake
+$ladder
 %
  
-#cp newdata.txt ${s2}_${l2}.dat
+cp newdata.txt ${snake}_${ladder}.dat
 
 nlines1=`expr $nlines - $n1 `
 tail -n $nlines1 temp.dat > temp1.dat
